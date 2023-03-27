5cebb1182d170eb0a6636a7e44eb24bd0f1539df
rithik <115656594+theRithik@users.noreply.github.com>
2023-03-27 20:21:51 +0530

    Add files via upload

---
+++ chechDB.js
@@ -0,0 +1,18 @@
+const { MongoClient} = require('mongodb')
+
+async function showDataBase(client){
+    const dataList = await client.db().admin().listDatabases();
+    console.log('dbs', dataList)
+     dataList.databases.forEach(db=> console.log(` -${db.name}`))
+}
+
+
+
+
+module.exports = async function connectToMongoDB(){
+    const url =  'mongodb://127.0.0.1:27017/'
+    const client = new MongoClient(url);
+    await client.connect()
+    await showDataBase(client)
+    await client.close()
+} 
